47fd18e30cda88a027ae9e48fd4916de2d8e76e6
Cristiano Sobral <cssobral2013@gmail.com>
2024-06-12 21:25:11 -0300

    Re-base to DM Sans and others

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # Wladi-Sans
-Is a modification of Poppins, version 3.010. Changes include double-storey 'a' (imported and adapted from https://github.com/usted/Albert-Sans), re-done metrics, removed Devanagari support, and remastered.
 
-Full list of changes from Poppins 3.010 (in no particular order):
-- Re-done vertical metrics
-- Imported 'a' from Albert Sans (https://github.com/usted/Albert-Sans)
-- Thickened 'e' crossbar in Regular
-- Imported S/s from Poppins 3.100 in Black/Black Italic
-- Re-interpolated 'S'/'s' to match previous change
-- Adapted 'ae'/'oe' to match the previous two changes
-- Changed 'i'/'j' tittles to rectangular
-- Added some Latin support
+Is developed from DM Sans, Figtree, and Unageo fonts. Changes include:
+
+ - Imported Greek/Cyrillic/Vietnamese support from both Figtree and Unageo fonts;
+
+ - Re-adapted vertical metrics;
+
+ - Flattened open-contour ‘3’, ‘4’, ‘6’, and ‘9’;
+
+ - Some other changes.
---
+++ fonts/generate-woff-woff2.py
@@ -0,0 +1,305 @@
+from fontTools.ttLib import TTFont
+
+'''Generate WOFF from TTF or OTF font.'''
+
+srcPath  = './ttf/WladiSans-ExtraLight.ttf'
+woffPath = './web/WladiSans-ExtraLight.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/WladiSans-ExtraLightItalic.ttf'
+woffPath = './web/WladiSans-ExtraLightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+    
+srcPath  = './ttf/WladiSans-Light.ttf'
+woffPath = './web/WladiSans-Light.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/WladiSans-LightItalic.ttf'
+woffPath = './web/WladiSans-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/WladiSans-LightItalic.ttf'
+woffPath = './web/WladiSans-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/WladiSans-Regular.ttf'
+woffPath = './web/WladiSans-Regular.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/WladiSans-Italic.ttf'
+woffPath = './web/WladiSans-Italic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/WladiSans-Medium.ttf'
+woffPath = './web/WladiSans-Medium.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/WladiSans-MediumItalic.ttf'
+woffPath = './web/WladiSans-MediumItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/WladiSans-SemiBold.ttf'
+woffPath = './web/WladiSans-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/WladiSans-SemiBoldItalic.ttf'
+woffPath = './web/WladiSans-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/WladiSans-SemiBold.ttf'
+woffPath = './web/WladiSans-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/WladiSans-SemiBoldItalic.ttf'
+woffPath = './web/WladiSans-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/WladiSans-Bold.ttf'
+woffPath = './web/WladiSans-Bold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/WladiSans-BoldItalic.ttf'
+woffPath = './web/WladiSans-BoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/WladiSans-ExtraBold.ttf'
+woffPath = './web/WladiSans-ExtraBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/WladiSans-ExtraBoldItalic.ttf'
+woffPath = './web/WladiSans-ExtraBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/WladiSans-Black.ttf'
+woffPath = './web/WladiSans-Black.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/WladiSans-BlackItalic.ttf'
+woffPath = './web/WladiSans-BlackItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+
+
+'''Generate WOFF2 from TTF or OTF font.'''
+
+srcPath  = './ttf/WladiSans-ExtraLight.ttf'
+woff2Path = './web/WladiSans-ExtraLight.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/WladiSans-ExtraLightItalic.ttf'
+woff2Path = './web/WladiSans-ExtraLightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+    
+srcPath  = './ttf/WladiSans-Light.ttf'
+woff2Path = './web/WladiSans-Light.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/WladiSans-LightItalic.ttf'
+woff2Path = './web/WladiSans-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/WladiSans-LightItalic.ttf'
+woff2Path = './web/WladiSans-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/WladiSans-Regular.ttf'
+woff2Path = './web/WladiSans-Regular.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/WladiSans-Italic.ttf'
+woff2Path = './web/WladiSans-Italic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/WladiSans-Medium.ttf'
+woff2Path = './web/WladiSans-Medium.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/WladiSans-MediumItalic.ttf'
+woff2Path = './web/WladiSans-MediumItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/WladiSans-SemiBold.ttf'
+woff2Path = './web/WladiSans-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/WladiSans-SemiBoldItalic.ttf'
+woff2Path = './web/WladiSans-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/WladiSans-SemiBold.ttf'
+woff2Path = './web/WladiSans-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/WladiSans-SemiBoldItalic.ttf'
+woff2Path = './web/WladiSans-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/WladiSans-Bold.ttf'
+woff2Path = './web/WladiSans-Bold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/WladiSans-BoldItalic.ttf'
+woff2Path = './web/WladiSans-BoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/WladiSans-ExtraBold.ttf'
+woff2Path = './web/WladiSans-ExtraBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/WladiSans-ExtraBoldItalic.ttf'
+woff2Path = './web/WladiSans-ExtraBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/WladiSans-Black.ttf'
+woff2Path = './web/WladiSans-Black.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/WladiSans-BlackItalic.ttf'
+woff2Path = './web/WladiSans-BlackItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
